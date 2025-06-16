17fd077f76e02a68b5399fe30b3f86d706c8d3ea
Natalia Zabolotnia <zanatav78@gmail.com>
2025-06-16 08:49:30 +0300

    change main.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,27 +1,53 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 export function createGallery(images) {
-    return images.map(({webformatURL,largeImageURL,tags ,likes,views,comments,downloads,})=>`
+  const gallery = document.querySelector('.gallery');
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
       <li class="list-item">
       <a href="${largeImageURL}">
       <img src="${webformatURL}" alt="${tags}" width="360"/>
       </a>
        <div class="info">
-          <p>Likes ${likes}</p>
-          <p>Views ${views}</p>
-          <p>Comments ${comments}</p>
-          <p>Downloads ${downloads}</p>
+            <p>Likes ${likes}</p>
+            <p>Views ${views}</p>
+            <p>Comments ${comments}</p>
+            <p>Downloads ${downloads}</p>
         </div>
         
       </li>  
-        `).join("");}
-export function clearGallery(){
-    const gallery=document.querySelector(".gallery");
-     gallery.innerHTML = "";
+        `
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
-export function showLoader(){
-   const loader=document.querySelector(".loader");
-   if (loader) loader.classList.remove("hidden");
+export function clearGallery() {
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = '';
 }
-export function hideLoader(){
-    const loader=document.querySelector(".loader");
-   if (loader) loader.classList.add("hidden");
+export function showLoader() {
+  const loader = document.querySelector('.loader');
+  if (loader) loader.classList.remove('hidden');
 }
+export function hideLoader() {
+  const loader = document.querySelector('.loader');
+  if (loader) loader.classList.add('hidden');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
--- src/main.js
+++ src/main.js
@@ -1,69 +1,51 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import {getImagesByQuery} from "./js/pixabay-api";
-import {createGallery} from "./js/render-functions"
-import {clearGallery} from "./js/render-functions"
-import {showLoader} from "./js/render-functions"
-import {hideLoader} from "./js/render-functions"
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { getImagesByQuery } from './js/pixabay-api';
+import { createGallery } from './js/render-functions';
+import { clearGallery } from './js/render-functions';
+import { showLoader } from './js/render-functions';
+import { hideLoader } from './js/render-functions';
 
+const input = document.querySelector('.input-text');
+const form = document.querySelector('.form');
 
-const input=document.querySelector(".input-text");
-const form=document.querySelector(".form");
-const gallery=document.querySelector(".gallery");
+form.addEventListener('submit', handlesubmit);
+async function handlesubmit(event) {
+  event.preventDefault();
+  const query = input.value.trim();
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  captions: true,
-  captionsData: 'alt',
-  captionDelay: 250,
-  captionPosition: 'bottom',
-});
+  clearGallery();
+  showLoader();
 
-
-async  function handlesubmit(event){
-event.preventDefault();
-const query=input.value.trim();
-
-
-clearGallery(); 
-showLoader(); 
-
-if (query === "") {
+  if (query === '') {
     iziToast.show({
-        title: 'Warning',
-        color: 'red',
-        position: "topRight" ,
-        message: 'Sorry, there are no images matching your search query. Please try again!'
+      title: 'Warning',
+      color: 'red',
+      position: 'topRight',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
     });
     return;
-}
+  }
 
-
-
-
-try {
+  try {
     const data = await getImagesByQuery(query);
-    if (data.hits.length=== 0) {
-        iziToast.show({
-            title: 'Hey',
-            color: 'red',
-            position: "topRight" ,
-            message: 'Sorry, there are no images matching your search query. Please try again!'       
-        });
-        return;
-}
+    if (data.hits.length === 0) {
+      iziToast.show({
+        title: 'Hey',
+        color: 'red',
+        position: 'topRight',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      return;
+    }
 
-
-const markup = createGallery(data.hits);
-gallery.insertAdjacentHTML("beforeend", markup);
-lightbox.refresh(); 
-} catch (error) {
-    console.error("Fetch error:", error);
-}
-finally {
+    createGallery(data.hits);
+  } catch (error) {
+    console.error('Fetch error:', error);
+  } finally {
     hideLoader();
-    input.value="";
+    input.value = '';
+  }
 }
-}
-form.addEventListener("submit",handlesubmit);
